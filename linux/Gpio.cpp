349cc924e7e74ccbd3ace10fbc594fc06163c41b
vortex314 <lieven.merckx@gmail.com>
2020-12-25 23:09:03 +0000

    Gpio interrupt

--- linux/Gpio.cpp
+++ linux/Gpio.cpp
@@ -1,7 +1,4 @@
 #include <Gpio.h>
-#ifdef __arm__
-#define HAS_GPIO 1
-#endif
 #ifdef HAS_GPIO
 #include <wiringPi.h>
 #else
@@ -14,18 +11,47 @@
   INFO("gpio value pin %d = %s ", __pin, __value)
 #endif
 
+Gpio *gpio[40];
+
+#define HANDLER(_X_)                     \
+  void handler##_X_(void)                \
+  {                                      \
+    INFO(" pin interrupt : %d ", _X_);   \
+    gpio[_X_]->value = digitalRead(_X_); \
+  }
+
+static uint8_t gpioRaspberry[] = {4, 9, 10, 17, 18, 22, 23, 24, 25, 27};
+HANDLER(4);
+HANDLER(9);
+HANDLER(10);
+HANDLER(17);
+HANDLER(18);
+HANDLER(22);
+HANDLER(23);
+HANDLER(24);
+HANDLER(25);
+HANDLER(27);
+
+Handler handler[] = {(Handler)0, (Handler)1, (Handler)2, (Handler)3, handler4, (Handler)5, (Handler)6, (Handler)7,
+                     (Handler)8, handler9, handler10,
+                     (Handler)11, (Handler)12, (Handler)13, (Handler)14, (Handler)15, handler17, handler18,
+                     (Handler)19, (Handler)20, (Handler)21, handler22, handler23, handler24, handler25,
+                     (Handler)26, handler27};
+
 void Gpio::init()
 {
 #ifdef HAS_GPIO
   wiringPiSetupGpio();
-#endif
+#else
   INFO(" this GPIO doesn't do anything !");
+#endif
 }
 
-Gpio::Gpio(int pin)
+Gpio::Gpio(Thread &thread, int pin) : _pollTimer(thread, 1000, true, "gpioPollTimer")
 {
   _pin = pin;
   _mode = M_INPUT;
+  gpio[_pin] = this;
 
   mode >> [&](const std::string &m) {
     _mode = m[0] == 'O' ? M_OUTPUT : M_INPUT;
@@ -38,7 +64,7 @@ Gpio::Gpio(int pin)
       pinMode(_pin, OUTPUT);
     mode = _mode == M_INPUT ? "INPUT" : "OUTPUT";
 #else
-    INFO(" GPIO %d ", pin);
+    INFO(" stub GPIO %d mode %s ", pin, _mode.c_str());
 #endif
   };
 
@@ -48,9 +74,26 @@ Gpio::Gpio(int pin)
 #ifdef HAS_GPIO
     if (_mode == M_OUTPUT)
       digitalWrite(_pin, _value ? HIGH : LOW);
+#else
+    INFO(" stub GPIO %d write %d ", pin, _value);
 #endif
     value = _value;
   };
+  _pollTimer >> [&](const TimerMsg &tm) {
+#ifdef HAS_GPIO
+    if (_mode == M_INPUT)
+      value = digitalRead(_pin);
+    else
+      value = _value;
+    INFO(" GPIO %d read %d ", _pin, value());
+#else
+    INFO(" stub GPIO %d read %d ", pin, _value);
+#endif
+  };
+#ifdef HAS_GPIO
+  if ((int)handler[_pin] > 100)
+    wiringPiISR(_pin, INT_EDGE_BOTH, handler[_pin]);
+#endif
 }
 
 Gpio::~Gpio() {}
--- linux/Gpio.h
+++ linux/Gpio.h
@@ -1,19 +1,32 @@
 #ifndef _GPIO_H_
 #define _GPIO_H_
 #include <limero.h>
+#include <wiringPi.h>
+
+
+
 class Gpio {
   int _pin;
 
  public:
   enum Mode { M_INPUT, M_OUTPUT };
-  Gpio(int);
+  Gpio(Thread& ,int);
   ~Gpio();
   ValueFlow<std::string> mode;
   ValueFlow<int> value;
   static void init();
+  void request();
 
  private:
   Mode _mode;
   int _value;
+  TimerSource _pollTimer;
 };
+
+extern Gpio* gpio[];
+typedef void (*Handler)(void);
+
+template < int P> 
+void GpioIsr(void) {
+  gpio[P]->value = digitalRead(gpio[P]->_pin);};
 #endif
