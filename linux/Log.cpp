62d71459ae31324e6267fac718ef1a4f0a2321f9
lieven <lieven.merckx@gmail.com>
2022-06-11 23:30:03 +0200

    Log

--- linux/Log.cpp
+++ linux/Log.cpp
@@ -9,12 +9,7 @@
 #include <ctime>
 #include <iomanip>
 
-<<<<<<< HEAD
 void consoleLogWriter(char *line, size_t length) {
-=======
-void consoleLogWriter(char *line, size_t length)
-{
->>>>>>> Log
   fwrite(line, length, 1, stdout);
   fwrite("\n", 1, 1, stdout);
 }
