4935f4acae12d251a56089da1eac487793a5dc33
lieven <lieven.merckx@gmail.com>
2022-09-11 17:20:52 +0200

    moved

---
+++ inc/MqttSerial.h
@@ -0,0 +1,58 @@
+#ifndef MQTTSERIAL_H
+#define MQTTSERIAL_H
+#include <Mqtt.h>
+#include <limero.h>
+#include <string>
+
+
+#define QOS 0
+#define TIMEOUT 10000L
+#define TIMER_KEEP_ALIVE 1
+#define TIMER_CONNECT 2
+#define TIMER_SERIAL 3
+
+class MqttSerial : public Mqtt {
+  StaticJsonDocument<3000> _jsonBuffer;
+  std::string  _clientId;
+  std::string  _address;
+  std::string  _lwt_topic;
+  std::string  _lwt_message;
+  Stream& _serial;
+
+ private:
+  StaticJsonDocument<256> txd;
+  StaticJsonDocument<256> rxd;
+  std::string  _rxdString;
+  std::string  _loopbackTopic;
+  uint64_t _loopbackReceived;
+
+  enum { CMD_SUBSCRIBE = 0, CMD_PUBLISH };
+
+  void handleSerialByte(uint8_t);
+  void rxdSerial(std::string  &);
+  void txdSerial(JsonDocument &);
+  void publish(std::string  topic, std::string  message);
+  void subscribe(std::string topic);
+
+ public:
+  static void onRxd(void *);
+  TimerSource keepAliveTimer;
+  TimerSource connectTimer;
+  MqttSerial(Thread &thr,Stream& serial);
+  ~MqttSerial();
+  void init();
+
+  void mqttPublish(const char *topic, const char *message);
+  //void mqttSubscribe(const char *topic);
+  void mqttConnect();
+  void mqttDisconnect();
+
+  bool handleMqttMessage(const char *message);
+  void observeOn(Thread &);
+
+  void on(const TimerMsg &);
+  void on(const MqttMessage &);
+  void request();
+};
+
+#endif  // MQTTSERIAL_H
